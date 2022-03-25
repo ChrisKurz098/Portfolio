3805ae75b2a459856d5f8cab0f09d1c4d17446b0
CKURZ <ckurz098@gmail.com>
2022-03-25 08:08:43 -0700

    Added project card data, styles and animations

--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -11,13 +11,13 @@ function buttonHandler(event) {
             <nav onClick={buttonHandler}>
                 <img id="profile-pic" src={portImg} alt="Developer Chris Kurz" />
 
-                <button className={`"inFromLeft-1" ${view === "About" && "nav-selected"}`} type='button' name="About" >About Me</button>
+                <button className={`inFromLeft-1 ${view === "About" && "nav-selected"}`} type='button' name="About" >About Me</button>
 
-                <button className={`"inFromLeft-2" ${view === "Work" && "nav-selected"}`} type='button' name="Work" >My Work</button>
+                <button className={`inFromLeft-2 ${view === "Work" && "nav-selected"}`} type='button' name="Work" >My Work</button>
 
-                <button className={`"inFromLeft-3" ${view === "Contact" && "nav-selected"}`} type='button' name="Contact" >Contact</button>
+                <button className={`inFromLeft-3 ${view === "Contact" && "nav-selected"}`} type='button' name="Contact" >Contact</button>
 
-                <button className={`"inFromLeft-4" ${view === "Resume" && "nav-selected"}`} type='button' name='Resume'>Resume</button>
+                <button className={`inFromLeft-4 ${view === "Resume" && "nav-selected"}`} type='button' name='Resume'>Resume</button>
                 <Footer/>
             </nav>   
     )
--- src/components/Projects/index.js
+++ src/components/Projects/index.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 
 
 function Projects() {
-    const projects = ['Plantzzz', 'CRT-Flashback-Machine'];
+    const projects = ['Plantzzz', 'TubeSim', 'EMPL-Tracker', 'Spelling-Bee','Weather-Dashborard'];
     const [projectData, setProjectData] = useState([]);
 
     useEffect(() => {
@@ -12,7 +12,8 @@ function Projects() {
 
     async function getProjects() {
         const projectArray = [];
-        await projects.forEach(async (project, i) => {
+        let index = 1;
+        for(const project of projects) {
             const response = await fetch(`https://api.github.com/repos/chriskurz098/${project}`);
             const data = await response.json();
             console.log('DATA: ', data)
@@ -20,22 +21,30 @@ function Projects() {
                 name: data.name,
                 description: data.description,
                 gitLink: data.git_url,
-                liveLink: data.homepage
+                liveLink: data.homepage,
+                index: index
             });
-
-            setProjectData([...projectArray]);
-        });
-        console.log('----done----')
-        
+            index++
+           
+        };
+        setProjectData(projectArray);
     }
 
     console.log('ProjectData: ', projectData)
 
     return (
-        <div>
+        <div className="project-card-container">
+            {!projectData && <p>Gathering Projects...</p>}
             {projectData.map((data) => (
-                <div key={data.name}>
-                    Name: {data.name}
+                <div key={data.index} className="project-card" style={{ animationDuration: `${data.index/2}s`}}>
+                    <h4>{data.name}</h4>
+                    <a href={data.gitLink}>
+                   <img src={require('../../assets/images/gitIcon.png')} alt="GitHub Link"/>
+                   </a>
+                   <br/>
+                   <p>{data.description}</p>
+                  
+                     {data.liveLink && <a className='liveLink' href={data.liveLink} target='__blank'>Live Link</a>}
                 </div>
             ))}
         </div>
