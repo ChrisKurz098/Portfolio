e631a46bfbcdcc8b8c509eeefc35e52bf70613ec
CKURZ <ckurz098@gmail.com>
2023-01-16 12:38:13 -0800

    Updated about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -8,7 +8,12 @@ const About = () => {
             </div>
             <div className='about-flex2'>
             <h2>About Me</h2>
-            <p>Full-Stack web developer with an extended background in digital media production, providing knowledge in visual design and a high attention to detail. Recently finished a certificate from the University of Oregon in Full-Stack Web Development. Strengths include punctuality, meeting deadlines, work ethic, teamwork and efficiency. Finds excitement in learning new things and working with like-minded individuals to solve problems and execute complex tasks.
+            <p style={{textAlign: 'left',  textIndent: '10%'}}>
+            I have always had a love for computer technology and as a kid I got my start in coding with HyperCard on my Macitosh Plus. It was a hand-me-down computer that was well beyond its prime, but its limitations inspired me try and make the most of what it could do. I would spend hours digging through The Complete HyperCard Handbook, a 900 page epic that documented everything you could do with the software, trying to find answers and solutions to make my ideas come to life.
+            </p>
+            <br/>
+            <p style={{textAlign: 'left', textIndent: '10%'}}>
+             I love solving problems and have a knack for finding solutions even when presented with unfamiliar tools. I can always find some way to make an idea come to fruition. I have a great ability to work under stress and tight deadlines and my ability to stay level headed in these circumstances helps create a productive and enjoyable environment for my teammates. My kindness and understanding of others make me very approachable for those seeking help and also allows me to be humble and accepting  when receiving constructive criticism and advice. 
             </p>
         </div>
         </section >
